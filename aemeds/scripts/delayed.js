bfae27657000d0903f68c82b315c7a61cfc48b9a
Andrei Tuicu <tuicu@adobe.com>
2024-04-03 17:30:49 +0200

    test: Adobe Launch Less Code - Post Render

--- aemeds/scripts/delayed.js
+++ aemeds/scripts/delayed.js
@@ -7,7 +7,7 @@ sampleRUM('cwv');
 async function loadAdobeDTM() {
   await loadScript(`${window.hlx.codeBasePath}/scripts/jquery-3.7.1.min.js`);
   const prod = 'https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-039be8795dc8.min.js';
-  const stage = 'https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-a2ae4c3b0523-staging.min.js';
+  const stage = 'https://assets.adobedtm.com/a441b904b50e/99538f40e7c0/launch-dfab9f712d3c-staging.min.js';
 
   const searchParams = new URLSearchParams(window.location.search);
   const env = searchParams.get('launch');
