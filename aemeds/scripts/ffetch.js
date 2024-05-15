28eaded9db931cef0e5fb29c736ae419a07120c4
Sergiu Coman <scoman@adobe.com>
2024-05-15 20:43:37 +0300

    revert caching

--- aemeds/scripts/ffetch.js
+++ aemeds/scripts/ffetch.js
@@ -17,12 +17,12 @@ async function* request(url, context) {
   for (let offset = 0, total = Infinity; offset < total; offset += chunkSize) {
     const params = new URLSearchParams(`offset=${offset}&limit=${chunkSize}`);
     if (sheetName) params.append('sheet', sheetName);
-      const resp = await fetch(`${url}?${params.toString()}`);
-      if (resp.ok) {
+    const resp = await fetch(`${url}?${params.toString()}`);
+    if (resp.ok) {
       const json = await resp.json();
-    total = json.total;
-    context.total = total;
-    for (const entry of json.data) yield entry;
+      total = json.total;
+      context.total = total;
+      for (const entry of json.data) yield entry;
     } else {
       return;
     }
