47c390751e29164ff8b7b293c74109c7f081bac8
Andrei Tuicu <tuicu@adobe.com>
2024-05-22 22:38:25 +0200

    Load Launch After LCP

--- aemeds/scripts/scripts.js
+++ aemeds/scripts/scripts.js
@@ -74,6 +74,40 @@ export async function fetchAPI(path) {
   return json;
 }
 
+async function loadAdobeDTM() {
+  const prod =
+    "https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-039be8795dc8.min.js";
+  const stage =
+    "https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-a2ae4c3b0523-staging.min.js";
+
+  const searchParams = new URLSearchParams(window.location.search);
+  if (window.location.pathname === '/blogs/2023/ai-business-process-analyst-role'
+    && searchParams.get("disableLaunch") === "true") {
+    return;
+  }
+
+  const env = searchParams.get("launch");
+  if (env === "prod") {
+    loadScript(prod, { async: "" });
+    return;
+  }
+
+  if (env === "stage") {
+    loadScript(stage, { async: "" });
+    return;
+  }
+
+  const { host } = window.location;
+  if (host === "servicenow.com" || host === "www.servicenow.com") {
+    loadScript(prod, { async: "" });
+  } else {
+    if (searchParams.get("disableLaunch") === "true") {
+      return;
+    }
+    loadScript(stage, { async: "" });
+  }
+}
+
 /**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
@@ -521,6 +555,8 @@ async function loadEager(doc) {
   } catch (e) {
     // do nothing
   }
+
+  loadAdobeDTM();
 }
 
 /**
