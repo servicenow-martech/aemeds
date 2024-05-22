97b74a886cb2c81fa79b11637f07769c8ab017ff
Andrei Tuicu <tuicu@adobe.com>
2024-05-22 21:11:47 +0200

    Load Loaunch After Rendering

--- aemeds/scripts/scripts.js
+++ aemeds/scripts/scripts.js
@@ -15,6 +15,7 @@ import {
   waitForLCP,
   loadBlock,
   readBlockConfig,
+  loadScript,
 } from './aem.js';
 import {
   a, div, p, span,
@@ -56,6 +57,40 @@ export function analyticsGlobalClickTrack(digitalData, event) {
   };
 
   window.appEventData.push(data);
+}
+
+async function loadAdobeDTM() {
+  const prod =
+    "https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-039be8795dc8.min.js";
+  const stage =
+    "https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-a2ae4c3b0523-staging.min.js";
+
+  const searchParams = new URLSearchParams(window.location.search);
+  if (window.location.pathname === '/blogs/2023/ai-business-process-analyst-role'
+    && searchParams.get("disableLaunch") === "true") {
+    return;
+  }
+
+  const env = searchParams.get("launch");
+  if (env === "prod") {
+    loadScript(prod, { async: "" });
+    return;
+  }
+
+  if (env === "stage") {
+    loadScript(stage, { async: "" });
+    return;
+  }
+
+  const { host } = window.location;
+  if (host === "servicenow.com" || host === "www.servicenow.com") {
+    loadScript(prod, { async: "" });
+  } else {
+    if (searchParams.get("disableLaunch") === "true") {
+      return;
+    }
+    loadScript(stage, { async: "" });
+  }
 }
 
 export async function fetchAPI(path) {
@@ -543,6 +578,7 @@ async function loadLazy(doc) {
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
+  loadAdobeDTM();
 
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
