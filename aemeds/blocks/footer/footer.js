18674c02a0662a82cc32044562239dbd4cd82f26
Balakrishnan_servicenow <balakrishnan.n@servicenow.com>
2025-06-17 21:51:40 +0530

    updated footer

--- aemeds/blocks/footer/footer.js
+++ aemeds/blocks/footer/footer.js
@@ -24,11 +24,29 @@ export default async function decorate(block) {
       }),
     );
 
-    // load NaaS footer code
     await Promise.all([
-      loadCSS(`${dataDomain}/nas/csi/footer/v1/footerCSR.bundle.css`),
-      loadScript(`${dataDomain}/nas/csi/footer/v1/footerCSR.bundle.js`),
+      loadScript(`${dataDomain}/nas/csi/naas.csr.bundles.versioning.init.js`),
     ]);
+
+    // // load NaaS footer code
+    // await Promise.all([
+    //   loadCSS(`${dataDomain}/nas/csi/footer/v1/footerCSR.bundle.css`),
+    //   loadScript(`${dataDomain}/nas/csi/footer/v1/footerCSR.bundle.js`),
+    // ]);
+
+    function injectNaasBundleScript(id, type, version, env, ext) {
+      const script = document.createElement('script');
+      script.id = id;
+      document.body.appendChild(script);
+
+      const event = new CustomEvent('naas-create-bundle', {
+        detail: { id, type, version, env, ext },
+      });
+      document.dispatchEvent(event);
+    }
+
+    injectNaasBundleScript('footer-bundle-css', 'footer', 'v1', dataDomain, 'css');
+    injectNaasBundleScript('footer-bundle-js', 'footer', 'v1', dataDomain, 'js');
 
     // trigger and wait for NaaS footer rendering
     await new Promise((resolve) => {
