df66c771c9d2d6a3f7edf41a367aadfae1be1b97
bala-servicenow <balakrishnan.n@servicenow.com>
2025-06-18 21:19:34 +0530

    Naasheaderv3 (#23)
    * naas header v3 update
    
    * code clean up
    
    * added fontawesome for header
    
    * updated footer
    
    * code clean up with lint
    
    * updated script with async attribute
    
    * code clean up
    
    * added async for script
    
    * code clean up
    
    * deferred the header and footer script
    
    * updated script to async

--- aemeds/blocks/footer/footer.js
+++ aemeds/blocks/footer/footer.js
@@ -1,8 +1,21 @@
-import { loadCSS, loadScript } from '../../scripts/aem.js';
+import { loadScript } from '../../scripts/aem.js';
 import { section } from '../../scripts/dom-helpers.js';
 import { getLocale } from '../../scripts/scripts.js';
 import { fixRelativeDAMImages, getDataDomain, waitImagesLoad } from '../header/header.js';
 
+export async function injectNaasBundleScript(id, type, version, env, ext) {
+  const script = document.createElement('script');
+  script.id = id;
+  script.setAttribute('async', '');
+  document.body.appendChild(script);
+
+  const event = new CustomEvent('naas-create-bundle', {
+    detail: {
+      id, type, version, env, ext,
+    },
+  });
+  document.dispatchEvent(event);
+}
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
@@ -24,11 +37,18 @@ export default async function decorate(block) {
       }),
     );
 
-    // load NaaS footer code
     await Promise.all([
-      loadCSS(`${dataDomain}/nas/csi/footer/v1/footerCSR.bundle.css`),
-      loadScript(`${dataDomain}/nas/csi/footer/v1/footerCSR.bundle.js`),
+      loadScript(`${dataDomain}/nas/csi/naas.csr.bundles.versioning.init.js`, { async: '' }),
     ]);
+
+    // // load NaaS footer code
+    // await Promise.all([
+    //   loadCSS(`${dataDomain}/nas/csi/footer/v1/footerCSR.bundle.css`),
+    //   loadScript(`${dataDomain}/nas/csi/footer/v1/footerCSR.bundle.js`),
+    // ]);
+
+    injectNaasBundleScript('footer-bundle-css', 'footer', 'v1', dataDomain, 'css');
+    injectNaasBundleScript('footer-bundle-js', 'footer', 'v1', dataDomain, 'js');
 
     // trigger and wait for NaaS footer rendering
     await new Promise((resolve) => {
--- aemeds/blocks/header/header.js
+++ aemeds/blocks/header/header.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-await-in-loop, no-continue */
-import { loadCSS, loadScript } from '../../scripts/aem.js';
+import { loadScript } from '../../scripts/aem.js';
 import { section } from '../../scripts/dom-helpers.js';
 import { getLocale } from '../../scripts/scripts.js';
 
@@ -65,6 +65,21 @@ export async function waitImagesLoad(block) {
   }
 }
 
+export async function injectNaasBundleScript(id, type, version, env, ext) {
+  const script = document.createElement('script');
+  script.id = id;
+  script.setAttribute('async', '');
+  document.head.appendChild(script);
+
+  const event = new CustomEvent('naas-create-bundle', {
+    detail: {
+      id, type, version, env, ext,
+    },
+  });
+
+  document.dispatchEvent(event);
+}
+
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -78,37 +93,37 @@ export default async function decorate(block) {
   try {
     block.append(
       section({
-        id: 'naas-header-v2',
+        id: 'naas-header-v3',
         class: 'cmp-nav__wrapper',
         'data-domain': dataDomain,
         'data-myaccount': 'hide',
         'data-search': 'hide',
-        'data-sourceId': 'blogs',
+        'data-sourceId': 'www',
         'data-lslinkshard': 'on',
-        'data-version': 'v2',
+        'data-version': 'v3',
+        role: 'banner',
+        'data-theme': 'dark',
+        'data-font-family': 'fontawesome',
       }),
     );
 
     // load NaaS header code
     await Promise.all([
-      loadCSS(`${dataDomain}/nas/csi/header/v2/headerV2CSR.bundle.css`),
-      loadScript(`${dataDomain}/nas/csi/header/v2/headerV2CSR.bundle.js`),
+      loadScript(`${dataDomain}/nas/csi/naas.csr.bundles.versioning.init.js`, { async: '' }),
     ]);
 
-    // trigger and wait for NaaS header rendering
-    await new Promise((resolve) => {
-      document.addEventListener('nass-header-rendered', () => {
-        fixRelativeDAMImages(block, dataDomain);
-        (async () => {
-          await waitImagesLoad(block);
-          resolve();
-        })();
-      });
+    injectNaasBundleScript('header-bundle-css', 'header', 'v3', dataDomain, 'css');
+    injectNaasBundleScript('header-bundle-js', 'header', 'v3', dataDomain, 'js');
 
-      document.dispatchEvent(new CustomEvent('naas-load-header'));
+    document.addEventListener('naas-load-header', () => {
+      const utilsScript = document.createElement('script');
+      utilsScript.src = `${dataDomain}/nas/common/consumers/blogs/naas-right-side-utils.v1.0.js`;
+      utilsScript.defer = true;
+      utilsScript.charset = 'UTF-8';
+      document.head?.appendChild(utilsScript);
     });
   } catch (e) {
     // eslint-disable-next-line no-console
-    console.error(e);
+    console.error('Failed to initialize NaaS header:', e);
   }
 }
